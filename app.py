637ea1bf71b374b27a6fd3bea824bea00a88c583
Duangkamon Kokanutaporn <mootuun_dk@hotmail.com>
2018-04-27 15:06:25 +0700

    add carcheck

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import tempfile
+import os
+import sys
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,11 +10,18 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage,VideoMessage,AudioMessage
 )
+
+from features.CarAnalytics import LicencePlate
 import priceOil
 
+
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 app = Flask(__name__)
+
+latest_image_path = ''
 # app = app.py
 line_bot_api = LineBotApi('BQOyunikRw/9QAmpvamgIxg62I23/9AXs1skmMDGx2M2gRpvdvAMwBF2y0PhczRnRQkQ9HQaobs0Faq+5wu2F0UdhL9/xN1owhJn4n3jhvLtYnosrXPEKuL3k/5mHelUKMNLFPp1bMYv0nX4WdGg9wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('9669a64ef354ce21d40b2dd7d774583f')
@@ -41,6 +51,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    global latest_image_path
     if (event.message.text == 'ราคาน้ำมัน') or (event.message.text == 'oilPrice') :
         l = priceOil.get_price()
         s = ""
@@ -50,11 +61,71 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=s))
-    else:
+    elif (event.message.text == 'ตรวจสอบรถ') :
+
         line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text='รอแปบ นะค่ะ')
+            ])
+        #process image
+        try:
+            lp = LicencePlate()
+            result = lp.process(latest_image_path)
+            s = lp.translate(result)
+
+            line_bot_api.push_message(
+                event.source.user_id, [
+                TextSendMessage(text=s),
+                ])
+        except Exception as e:
+            print('Exception:',type(e),e)
+            line_bot_api.push_message(
+                event.source.user_id, [
+                TextSendMessage(text='ไม่สามารถตรวจสอบได้ค่ะ รูปอาจจะมีความระเอียดต่ำไป')
+                ])
+
+    else:
+        line_bot_api.push_message(
             event.reply_token,
             # TextSendMessage(text=event.message.text+'จ๋าาา'))
             TextSendMessage(text='เราไม่เข้าใจนายอะ ลองใส่เป็น ราคาน้ำมัน หรือ oilPrice นะค่ะ'))
+
+
+
+
+
+
+# Other Message Type  s
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    global latest_image_path
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    else:
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+        dist_path = tempfile_path + '.' + ext
+        dist_name = os.path.basename(dist_path)
+        os.rename(tempfile_path, dist_path)
+    
+
+        #save image path
+        latest_image_path = dist_path
+        line_bot_api.reply_message(
+            event.reply_token,[
+                TextSendMessage(text= 'เก็บรูปให้แล้วค่ะ')
+            ])
+
 
 
 if __name__ == "__main__":
---
+++ features/CarAnalytics.py
@@ -0,0 +1,124 @@
+import requests
+import json
+class LicencePlate:
+    def __init__(self):
+        pass
+
+    def _process_json(self,jObject):
+        # print('Type:',type(jObject))
+        l = json.dumps(jObject)
+        l = l.replace("'",'"')
+        l = l.replace("False",'false')
+        l = l.replace("True",'true')
+
+        obj = json.loads(l)
+
+        output = {}
+        for r in obj['results']:
+            p = str(r['plate'])
+            output['Plate'] = p
+            v = r['vehicle']
+            make = v['make']
+            makes = []
+            for m in make:
+                if m['confidence'] > 50:
+                    # print('Make:',m['name'],m['confidence'])
+                    mdict = {'make':m['name'],'confidence':m['confidence']}
+                    makes.append(mdict)
+            output["make"] = makes
+
+            model = v['make_model']
+            models = []
+            # for m in model:
+            top = 2
+            for i in range(len(model)):
+                if top == 0:
+                    break
+                m = model[i]
+                top -= 1
+                # print('Model:',m['name'],m['confidence'])
+                mdict = {'model':m['name'],'confidence':m['confidence']}
+                models.append(mdict)
+            output['model'] = models
+
+            color = v['color']
+            colors = []
+            # for c in color:
+            top = 2
+            for i in range(len(color)):
+                if top == 0:
+                    break
+                c = color[i]
+                top -= 1
+                # print('Color:',c['name'],c['confidence'])        
+                cdict = {'color':c['name'],'confidence':c['confidence']}
+                colors.append(cdict)
+            output['color'] = colors
+        return output
+        
+
+    def process(self,filename):
+        #form Http request
+        # curl -X POST -F image=@test01.jpg "https://api.openalpr.com/v2/recognize?recognize_vehicle=1&country=th&secret_key=sk_e10fd59fb8ae37caf154ae5e"
+        v = '1'
+        c = 'th'
+        sk = "sk_6081b8410a10af89a1ce800e"
+
+        url = "https://api.openalpr.com/v2/recognize?recognize_vehicle=%s&country=%s&secret_key=%s"%(v,c,sk)
+        #filename = 'car_01.jpg'
+        #call ALPR API
+        r = requests.post(url, files={'image': open(filename,'rb')})
+        #obtain JSON result
+        #process JSON result
+        #return r.json()
+        result = self._process_json(r.json())
+        return result
+
+    def translate(self,data):
+        p = data['Plate']
+        s = 'ทะเบียนรถคือ %s\n' % (p)
+        # How many makes?
+        if len(data['make']) > 1:
+            s += 'ยี่ห้อที่เป็นไปได้คือ '
+            for m in data['make']:
+                s += m['name'] + ' '
+        else:
+            s += 'ยี่ห้อ %s' % (data['make'][0]['make'])
+
+        # How many model
+        if len(data['model']) > 1:
+            s += '\nรุ่นที่เป็นไปได้ คือ\n'
+            for m in data['model']:
+                if m['confidence'] > 50:
+                    s += m['model'] + '\n'
+                else:
+                    s += m['model'] + ' (ไม่มั่นใจ)\n'
+        else:
+            s += '\nรุ่น %s' % (data['model'][0]['model'])
+
+        # How many colors
+        if len(data['color']) > 0:
+            if len(data['color']) > 1:
+                s += '\nสีที่เป็นไปได้ คือ\n'
+                for m in data['color']:
+                    if m['confidence'] > 50:
+                        s += m['color'] + '\n'
+                    else:
+                        s += m['color'] + ' (ไม่มั่นใจ)\n'
+            else:
+                s += '\nสี %s' % (data['color'][0]['color'])
+
+        return s
+
+if __name__ == '__main__':
+    lp = LicencePlate()
+    filename = 'car_01.jpg'
+    result = lp.process(filename)
+    print(result)
+
+    s = lp.translate(result)
+    print(s)
+
+
+
+    
